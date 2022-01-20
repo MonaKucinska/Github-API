4280a11c3e8781eb32d3e62dc3beeeed1637c914
MonaKucinska <87846652+MonaKucinska@users.noreply.github.com>
2022-01-20 12:41:22 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const form = document.getElementById('newForm');
 const searchBtn = document.getElementById('searchBtn');
 const result = document.getElementById('result');
 const userName = document.getElementById('userName');
@@ -6,20 +5,26 @@ const userInfo = document.getElementById('userInfo');
 const userRepos = document.getElementById('userRepos');
 const ul = document.querySelector('ul');
 
+// clears area from the currently viewed profile (user info + repositories)
+const clearArea = () => {
+  if(userInfo.hasChildNodes()){
+    while(userInfo.firstChild) {
+      userInfo.removeChild(userInfo.lastChild);
+    }
+  }
+  if(ul.hasChildNodes()){
+    while(ul.firstChild) {
+      ul.removeChild(ul.lastChild);
+    }
+  }
+}
+
 // returns the input value from the user 
 const getUserName = () => {
   let user = userName.value;
   //remove spaces placed between words
   user = user.split(' ').join('');
   return user;
-}
-
-//gets username from getUserName function 
-//and forwards it to functions that fetch data from the GitHub API
-const searchUser = () => {
-  const userInput = getUserName();
-  fetchData(userInput);
-  fetchRepos(userInput);
 }
 
 //fetch basic GitHub user info and forwards it into printing function
@@ -31,80 +36,88 @@ const fetchData = async (userName) => {
       console.log("SUCCESS")
     }
     else {
-      console.log("NOT SUCCESS")
+      console.log("ERROR")
     }
     const result = await response.json();
-    printUserInfo(result);
+    return result;
   } catch(err) {
     console.error(err);
   }
-  
 }
 
 // prints info about user
 const printUserInfo = (user) => {
-   userInfo.innerHTML = `
-   <div class="image">
-    <img src='${user.avatar_url}' alt='User photo' />
+  userInfo.innerHTML = `
+  <div class="image">
+   <img src='${user.avatar_url}' alt='User photo' />
+  </div>
+  <div class="user">
+   <div class="card">
+     <h2>${user.login}</h2>
+     <p class="date">${user.created_at.substr(0, 10)}</p>
    </div>
-   <div class="user">
-    <div class="card">
-      <h2>${user.login}</h2>
-      <p class="date">${user.created_at.substr(0, 10)}</p>
-    </div>
-    <div class="card bio">
-      <h3>Bio</h3>
-      <p>${user.bio}</p>
-    </div>
-    <div class="card repos">
-      <div class="card-element">
-        <h3>Repositories</h3>
-        <p>${user.public_repos}</p>
-      </div>
-      <div class="card-element">
-        <h3>Followers</h3>
-        <p>${user.followers}</p>
-      </div>
-      <div class="card-element">
-        <h3>Following</h3>
-        <p>${user.following}</p>
-      </div>
-    </div>
-    <div class="card info">
-      <div class="card-element">
-        <i class="fas fa-map-marker-alt"></i>
-        <p>${user.location}</p>
-      </div>
-      <div class="card-element">
-        <i class="fas fa-envelope"></i>
-        <p>${user.email}</p>
-      </div>
-      <div class="card-element">
-        <i class="fas fa-building"></i>
-        <p>${user.company}</p>
-      </div>
-      <div class="card-element">
-        <i class="fab fa-twitter"></i>
-        <p>${user.twitter_username}</p>
-      </div>
-    </div>
+   <div class="card bio">
+     <h3>Bio</h3>
+     <p>${user.bio}</p>
    </div>
-   `
+   <div class="card repos">
+     <div class="card-element">
+       <h3>Repositories</h3>
+       <p>${user.public_repos}</p>
+     </div>
+     <div class="card-element">
+       <h3>Followers</h3>
+       <p>${user.followers}</p>
+     </div>
+     <div class="card-element">
+       <h3>Following</h3>
+       <p>${user.following}</p>
+     </div>
+   </div>
+   <div class="card info">
+     <div class="card-element">
+       <i class="fas fa-map-marker-alt"></i>
+       <p>${user.location}</p>
+     </div>
+     <div class="card-element">
+       <i class="fas fa-envelope"></i>
+       <p>${user.email}</p>
+     </div>
+     <div class="card-element">
+       <i class="fas fa-building"></i>
+       <p>${user.company}</p>
+     </div>
+     <div class="card-element">
+       <i class="fab fa-twitter"></i>
+       <p>${user.twitter_username}</p>
+     </div>
+   </div>
+  </div>
+  `
 }
+
+
 
 //fetch data about repositories and forwards it into printing function
 const fetchRepos = async (userName) => {
   try {
     const url = 'https://api.github.com/users/'+userName+'/repos';
     const response = await fetch(url);
+    if(response.ok){
+      console.log("SUCCESS")
+    }
+    else {
+      console.log("ERROR")
+    }
     const result = await response.json();
     const sortedRepos = sortRepos(result);
-    printRepos(sortedRepos);
+    return sortedRepos;
   } catch (err) {
     console.error(err);
   }
   
 }
+
 
 // prints data about repositories
 const printRepos = (repo) => {
@@ -141,21 +154,11 @@ const compareStars = (num1, num2) => {
   return 0;
 }
 
-// clears area from the currently viewed profile (user info + repositories)
-const clearArea = () => {
-  if(userInfo.hasChildNodes()){
-    while(userInfo.firstChild) {
-      userInfo.removeChild(userInfo.lastChild);
-    }
-  }
-  if(ul.hasChildNodes()){
-    while(ul.firstChild) {
-      ul.removeChild(ul.lastChild);
-    }
-  }
+const searchUser = () => {
+  const userInput = getUserName();
+  fetchData(userInput).then((result) => printUserInfo(result));
+  fetchRepos(userInput).then((sortedRepos) => printRepos(sortedRepos));
 }
-
-
 
 // search user after clicking a search button
 searchBtn.addEventListener("click", function(e){
